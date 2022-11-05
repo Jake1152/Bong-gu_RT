c3f9eec98fa6257e8516828f88d24eba532fdd2f
min-jo <mingi.jo@zeroone.ai>
2022-11-05 13:02:35 +0900

    fix: 개행 없는 rt 파일 파싱 가능
    TODO
    - 카메라 위 바라보면 외적 안 됨

--- src/parse_rt2.c
+++ src/parse_rt2.c
@@ -6,7 +6,7 @@
 /*   By: min-jo <min-jo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/16 18:29:12 by min-jo            #+#    #+#             */
-/*   Updated: 2022/11/03 23:53:18 by min-jo           ###   ########.fr       */
+/*   Updated: 2022/11/05 13:01:57 by min-jo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,10 @@ int	check_last(t_mlx *mlx, t_parse state, char c)
 		mlx->camera = newCamera(mlx->parse_cam.pos, mlx->parse_cam.ori);
 		return (0);
 	}
+	else if (state == PARSE_RT_AMB_B || state == PARSE_RT_CYL_B
+		|| state == PARSE_RT_LIG_B || state == PARSE_RT_PLA_B
+		|| state == PARSE_RT_SPH_B)
+		return (0);
 	else
 		return (1);
 }
